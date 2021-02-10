c375c075a0b45cbd4a38d1ecaabd02b14bb7ad3a
litchireda <litchi.reda@gmail.com>
2021-02-10 17:28:54 +0100

    Done? added resume, tweaked a few styling thingies

--- scripts/my_script.js
+++ scripts/my_script.js
@@ -3,15 +3,44 @@ $(document).ready(function(){
     $(window).scroll(function() {
         if ($(this).scrollTop() < 100) {
             $('.navbar').fadeOut();
-            console.log('lol');
         } else {
             $('.navbar').fadeIn();
-            console.log('xd')
         }
     });
 });
 
+
+
 particlesJS.load('particles-js', 'scripts/particles.json',
-        function(){
-            console.log('particles.json loaded...');
-        })
+    function(){
+        console.log('particles.json loaded...');
+    }
+);
+
+function isElementVisible($elementToBeChecked)
+{
+    var TopView = $(window).scrollTop();
+    var BotView = TopView + $(window).height();
+    var TopElement = $elementToBeChecked.offset().top;
+    var BotElement = TopElement + $elementToBeChecked.height();
+    return ((BotElement <= BotView) && (TopElement >= TopView));
+}
+
+$(window).scroll(function () {
+    $( ".skills-bar" ).each(function() {
+        $this = $(this);
+        isOnView = isElementVisible($(this));
+        if(isOnView && !$(this).hasClass('bar-animation')){
+            $(".php-bar").addClass('bar-animation');
+            $(".php-bar").removeClass('empty-bar');
+            $(".node-bar").addClass('bar-animation');
+            $(".node-bar").removeClass('empty-bar');
+            $(".react-bar").addClass('bar-animation');
+            $(".react-bar").removeClass('empty-bar');
+            $(".ruby-py-bar").addClass('bar-animation');
+            $(".ruby-py-bar").removeClass('empty-bar');
+            $(".adapt-bar").addClass('bar-animation');
+            $(".adapt-bar").removeClass('empty-bar');
+        }
+    });
+});
